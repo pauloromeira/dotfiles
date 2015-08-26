1b9ecd2e81b5ff51c203785f300f5b4fa89c2d5b
Paulo Romeira <paulo@pauloromeira.com>
2015-08-26 16:53:01 -0300

    Make vim run on bootstrap to load plugins

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,4 @@
-# TO DO: download vim-plug
 # TO DO: clone my forks to a specific directory (change .vimrc too)
-# TO DO: run vim +Pluginstall +qall (junegunn)
+
+# Running vim for the first time will download plug (manager) and install plugins
+vim +qall
