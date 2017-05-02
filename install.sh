abc37421c0c42c6cdad06538fb41c774d88916dd
Paulo Romeira <paulo@pauloromeira.com>
2017-05-02 19:30:37 -0300

    Add Oh My Zsh to global installation

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-# Pyenv
 if command -v curl > /dev/null; then
-  curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+  COMMAND='curl -fsSL'
 else
-  wget -O- https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+  COMMAND='wget -O -'
 fi
+
+# Oh My Zsh
+sh -c "$($COMMAND https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Pyenv
+sh -c "$($COMMAND https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer)"
