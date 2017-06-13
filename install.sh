3e6899381f431075623af8c2387c5b1134d22be6
Paulo Romeira <paulo@pauloromeira.com>
2017-06-12 21:54:38 -0300

    Comment zshrc installation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ else
 fi
 
 # Oh My Zsh
-sh -c "$($COMMAND https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$($COMMAND https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Pyenv
 sh -c "$($COMMAND https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer)"
