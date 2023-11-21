f001a5c0d8ed76ab152ff4f1bd82a406a11ea384
Paulo Romeira <paulo@pauloromeira.com>
2023-11-21 08:58:50 -0300

    bash: Add @fedora specific for bashrc

--- stow.sh
+++ stow.sh
@@ -2,8 +2,8 @@
 
 OS_ID=$(cat /etc/os-release  | grep -Poh "^ID=\K.*")
 
-stow --no-folding --adopt --verbose --ignore='^@.*' */
+stow --no-folding --adopt --verbose --ignore='^@.*' -t $HOME */
 
 for dir in */@$OS_ID; do
-  stow --no-folding --adopt --verbose -d ${dir%/*} @$OS_ID
+  stow --no-folding --adopt --verbose -d ${dir%/*} -t $HOME @$OS_ID
 done
