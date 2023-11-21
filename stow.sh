f37e74c310d7c8f5a7785346a91cdb18e5678b3c
Paulo Romeira <paulo@pauloromeira.com>
2023-11-21 08:58:17 -0300

    stow.sh: Add specific os logic

--- stow.sh
+++ stow.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-stow --no-folding --adopt --verbose */
+
+OS_ID=$(cat /etc/os-release  | grep -Poh "^ID=\K.*")
+
+stow --no-folding --adopt --verbose --ignore='^@.*' */
+
+for dir in */@$OS_ID; do
+  stow --no-folding --adopt --verbose -d ${dir%/*} @$OS_ID
+done
